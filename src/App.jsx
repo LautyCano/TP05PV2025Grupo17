60894e85a42c32ac8fd99747a88c966522954497
Lucas2007040507 <capitanchorizo1234@gmail.com>
2025-05-25 21:43:29 -0300

    feat: Se agrega la barra de navegacion[Lucas Luciano Camino]

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import NavBar from './components/NavBar';
+import AppRoutes from './Routes';
 
 function App() {
-  
   return (
     <>
+      <NavBar />
+      <div className="main-content">
+        <AppRoutes />
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
